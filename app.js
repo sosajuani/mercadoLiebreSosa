f963cc079d989586106ee1a70f65bd8d5cf51ff0
sosajuani <sosajuanignacio17@gmail.com>
2021-11-23 11:12:34 -0300

    modificacion de heroku

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const path = require('path');
 
 //carpeta publica
@@ -9,4 +8,4 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'public/views/home.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname,'public/views/login.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname,'public/views/register.html')));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT || 3000, () => console.log('servidor funcionando'));
